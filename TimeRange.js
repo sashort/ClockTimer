e07780401033aca9e1e391605ee5d98c810cdc01
sashort <95879519+sashort@users.noreply.github.com>
2026-09-11 02:59:44 +0000

    Add TimeRange split method

--- TimeRange.js
+++ TimeRange.js
@@ -729,6 +729,146 @@ class TimeRange extends HTMLElement {
         }
     }
 
+    split(
+        timeValue,
+        insert = false
+    ) {
+        if (
+            typeof insert !== "boolean" ||
+            !(this.#startTime instanceof Date) ||
+            !(this.#endTime instanceof Date)
+        ) {
+            return;
+        }
+
+        const splitTime =
+            this.#uniformDate(
+                timeValue,
+                false
+            );
+
+        if (!(splitTime instanceof Date)) {
+            return;
+        }
+
+        const startMilliseconds =
+            this.#startTime.getTime();
+
+        const endMilliseconds =
+            this.#endTime.getTime();
+
+        const splitMilliseconds =
+            splitTime.getTime();
+
+        if (
+            splitMilliseconds <= startMilliseconds ||
+            splitMilliseconds >= endMilliseconds
+        ) {
+            return;
+        }
+
+        const parent =
+            this.parentElement;
+
+        if (insert && !parent) {
+            return;
+        }
+
+        const originalEnd =
+            new Date(endMilliseconds);
+
+        const hadRangeLength =
+            this.hasAttribute(
+                "range-length"
+            );
+
+        const right =
+            document.createElement(
+                this.localName
+            );
+
+        for (const attribute of this.attributes) {
+            if (
+                attribute.name === "start-time" ||
+                attribute.name === "end-time" ||
+                attribute.name === "range-length" ||
+                attribute.name === "ignore-overlaps"
+            ) {
+                continue;
+            }
+
+            right.setAttribute(
+                attribute.name,
+                attribute.value
+            );
+        }
+
+        right.setAttribute(
+            "start-time",
+            this.#formatDateTime(
+                splitTime
+            )
+        );
+
+        right.setAttribute(
+            "end-time",
+            this.#formatDateTime(
+                originalEnd
+            )
+        );
+
+        if (hadRangeLength) {
+            right.setAttribute(
+                "range-length",
+                this.#formatRangeLength(
+                    endMilliseconds -
+                    splitMilliseconds
+                )
+            );
+        }
+
+        right.setAttribute(
+            "ignore-overlaps",
+            ""
+        );
+
+        right.timeRangeFullEntry =
+            true;
+
+        const transitioned =
+            this.transitionTo({
+                startTime:
+                    new Date(
+                        startMilliseconds
+                    ),
+                endTime:
+                    splitTime
+            });
+
+        if (!transitioned) {
+            return;
+        }
+
+        if (
+            typeof this.snapToLogicalTiming ===
+                "function"
+        ) {
+            this.snapToLogicalTiming();
+        }
+
+        if (insert) {
+            parent.insertBefore(
+                right,
+                this.nextSibling
+            );
+        }
+
+        return [
+            this,
+            right
+        ];
+    }
+
     stopElapsedAnimation() {
         if (
             this.getAttribute(
