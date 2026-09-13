0dbe0fc54dec697cf09eee56f4fbacf125e8c065
sashort <95879519+sashort@users.noreply.github.com>
2026-09-13 19:39:32 -0400

    Add API configuration template

---
+++ api/config.example.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'database' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'name' => 'trip_management',
+        'username' => 'clocktimer_api',
+        'password' => 'replace-me',
+    ],
+];
