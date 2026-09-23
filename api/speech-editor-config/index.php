f27d2b913f9fb163a6fa7932cb54a7228e3513ff
sashort <95879519+sashort@users.noreply.github.com>
2026-09-23 10:21:33 -0400

    Authorize Speech Editor config through delegated tokens

--- api/speech-editor-config/index.php
+++ api/speech-editor-config/index.php
@@ -82,21 +82,25 @@ $renderRegistry = static function (array $roles): string {
     return "globalThis.WMOFSpeechFunctionRoles = " . $encoded . ";\n";
 };
 
-authenticated_user_id();
-require_csrf();
+$authorization = authorize_guarded_access(
+    $method === 'GET'
+        ? [
+            PERMISSION_DEVELOPER_PREVIEW,
+            PERMISSION_DEVELOPER
+        ]
+        : [
+            PERMISSION_DEVELOPER
+        ],
+    ACCESS_TOKEN_SCOPE_SPEECH_EDITOR
+);
 
-if ($method === 'GET') {
-    require_any_permission(
-        PERMISSION_DEVELOPER_PREVIEW,
-        PERMISSION_DEVELOPER
-    );
-
-    json_response(array_merge($read(), $readRegistry()));
+if (guarded_access_requires_csrf($authorization)) {
+    require_csrf();
 }
 
-require_permission(
-    PERMISSION_DEVELOPER
-);
+if ($method === 'GET') {
+    json_response(array_merge($read(), $readRegistry()));
+}
 
 $input = json_input();
 $entries = $input['entries'] ?? null;
